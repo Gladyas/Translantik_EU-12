01a5e584933e6efeec47104a2462eff26abfdd14
gladyas <taltindasforum@gmail.com>
2023-03-29 21:50:45 +0300

    Translantik Cukes Runner and Hooks

---
+++ src/test/java/com/translantik/step_definitions/Hooks.java
@@ -0,0 +1,8 @@
+package com.translantik.step_definitions;
+
+public class Hooks {
+    public void setupScenario(){
+        System.out.println("====Setting up browser using cucumber @Before");
+
+    }
+}
